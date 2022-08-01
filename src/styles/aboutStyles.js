ca5fe6ddade218ef5aff0b47930efeeb0ac1980f
Amil Utkarsh Gupta <amiluguptaa@gmail.com>
2022-08-02 00:49:41 +0530

    made info better

--- src/styles/aboutStyles.js
+++ src/styles/aboutStyles.js
@@ -3,7 +3,7 @@ import { makeStyles } from "@mui/styles";
 const useStyles = makeStyles({
     about:{
         border: '.2vw solid aquamarine',
-        borderRadius: '5%',
+        borderRadius: '1rem',
         margin: '1rem 1rem',
         height: '100%',
     }
